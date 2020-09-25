3bc301e4be0c63bf6e9e63412912626a60d7e470
Nicolas <nicolas.lecrique+git@globers.co>
2020-09-25 17:09:05 +0200

    add program printing
    Former-commit-id: a4aa1cd4803156caef42884ef28520c864749836

--- programbuilder/build_programs.py
+++ programbuilder/build_programs.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
+from typing import Dict
 
 from languages_toolboxes.vietnamese_toolbox import VietnameseToolbox
-from program_data_builder import build_program_data, LanguageProgramData
+from program_data_builder import build_program_data, LanguageProgramData, LearnableWordData, LearnableSentenceData, \
+    ExactSentenceData
 
 
 @dataclass(frozen=True)
@@ -10,4 +12,55 @@ class LanguageProgram:
 
 
 program_data: LanguageProgramData = build_program_data("vietnamese", VietnameseToolbox())
-print(program_data)
+
+word_index = 0
+max_word_index = 2000
+
+list_words = list(program_data.learnable_word_to_data.items())
+sorted_word_list = sorted(list_words, key= lambda pair: -pair[1].count_in_corpus)
+word_data: LearnableWordData
+for word, word_data in sorted_word_list:
+    print("=============")
+    print("=============")
+    print("=============")
+    print(word + ": " + str(word_data.count_in_corpus))
+    print("=============")
+    print("=============")
+    print("=============")
+
+    sentence_index = 0
+    max_sentence_index = 20
+    word_index+=1
+    if word_index == max_word_index:
+        break
+
+    learnable_sentence_to_data: Dict[str, LearnableSentenceData] = word_data.learnable_sentence_to_data
+    sentence_list = list(learnable_sentence_to_data.items())
+    sorted_sentence_list = sorted(sentence_list, key= lambda pair: -pair[1].count_in_corpus)
+    sentence_data: LearnableSentenceData
+    for sentence, sentence_data in sorted_sentence_list:
+
+        print("====")
+        print(sentence + ": " + str(sentence_data.count_in_corpus))
+        print("====")
+        sentence_index+=1
+        if sentence_index == max_sentence_index:
+            break
+
+
+        exact_sentence_to_data: Dict[str, ExactSentenceData] = sentence_data.exact_sentence_to_data
+        exact_sentence_list = list(exact_sentence_to_data.items())
+        sorted_exact_sentence_list = sorted(exact_sentence_list, key= lambda pair: -pair[1].count_in_corpus)
+        exact_sentence_data: ExactSentenceData
+
+        max_exact=3
+        exact_index = 0
+
+        for exact_sentence, exact_sentence_data in sorted_exact_sentence_list:
+            exact_index+=1
+            if exact_index == max_exact:
+                break
+            print(exact_sentence + ": " + str(exact_sentence_data.count_in_corpus))
+
+
+
