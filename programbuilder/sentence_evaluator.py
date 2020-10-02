5f1f464afbed6da9fc83e69f990a95d245bf5bd7
Nicolas <nicolas.lecrique+git@globers.co>
2020-10-02 17:48:23 +0200

    temp state before trying to add masked end sequence token
    Former-commit-id: 3d5383727b7ba2fa81dccd27d7103b61c6d6e2b1

--- programbuilder/sentence_evaluator.py
+++ programbuilder/sentence_evaluator.py
@@ -1,14 +1,22 @@
+from dataclasses import dataclass
 from typing import List
 import numpy as np
-from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForMaskedLM, XLMRobertaTokenizer, \
-    XLMRobertaForMaskedLM, pipeline
+from transformers import AutoTokenizer, AutoModelForMaskedLM, XLMRobertaTokenizer, \
+    XLMRobertaForMaskedLM
+import torch
 
 # TODO NICO: tester utiliser phobert car sinon c'est trop facile: quand 2 mots forment un seul mot
 #le produit des 2 probas est proche de 1 alors que c'etait peut etre très dur de deviner ce mot
 
+@dataclass
 class Sentence:
     sentence: str
     words: [str]
+
+@dataclass
+class SentenceEvaluationResult:
+    sentence_proba: float
+    proba_by_word: [float]
 
 class SentenceEvaluatorTwo:
 
@@ -16,105 +24,62 @@ class SentenceEvaluatorTwo:
         self.tokenizer: XLMRobertaTokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
         self.model: XLMRobertaForMaskedLM = AutoModelForMaskedLM.from_pretrained("xlm-roberta-large")
 
-    def evaluate_sentence(self, sentences: [Sentence]) -> [float]:
+    # return tensor of (nb_word,nb_tokens) where each tokens of row i are masked
+    def _build_encoded_sentence_masked_by_word(
+            self, encoded_sentence: torch.tensor, encoded_words: [torch.tensor]) -> torch.tensor:
 
-        for sentence in sentences:
-            encoded_sentence = self.tokenizer.encode(sentence.sentence)
-            encoded_words = self.tokenizer.encode([word for word in sentence.words])
+        encoded_sentence_for_masks = encoded_sentence.repeat(len(encoded_words), 1)
+        current_start_index = 1
+        for idx_word, encoded_word in enumerate(encoded_words):
+            end_index = current_start_index + encoded_word.shape[1]
+            encoded_sentence_for_masks[idx_word, current_start_index:end_index] = self.tokenizer.mask_token_id
+            current_start_index = end_index
 
+        return encoded_sentence_for_masks
 
+    # return array of size nb_sentences, each row contains a numpy array of size nb_words_in_sentence with log_proba in each cell
+    def evaluate_sentence_log_proba_by_word(self, sentences: [Sentence]) -> [np.ndarray]:
 
+        with torch.no_grad():
 
+            probas = []
+            for sentence in sentences:
+                encoded_words = [self.tokenizer.encode(word, add_special_tokens=False, return_tensors="pt") for word in sentence.words]
+                encoded_sentence = self.tokenizer.encode(sentence.sentence, return_tensors="pt")
 
+                nb_words = len(sentence.words)
+                encoded_sentence_for_masks = self._build_encoded_sentence_masked_by_word(encoded_sentence, encoded_words)
 
+                result_tensor = self.model(encoded_sentence_for_masks)[0]
+                tensor_log_probs = torch.nn.functional.log_softmax(result_tensor, dim=2)
 
-class VietnameseSentenceEvaluator:
-    #self.tokenizer: XLMRobertaTokenizer = AutoTokenizer.from_pretrained("vinai/phobert-large")
-    #self.model: XLMRobertaForMaskedLM = AutoModelForMaskedLM.from_pretrained("vinai/phobert-large")
-    pass
-
-
-# TODO: le code marche pas car les mots sont découpés avec xlm-roberta: par exemple: couche devient "_cou", "che",
-# il faut gérer le cas ou le découpage se fait au milieu d'un mot
-
-class SentenceEvaluator:
-
-    def __init__(self):
-        self.tokenizer: XLMRobertaTokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
-        self.model: XLMRobertaForMaskedLM = AutoModelForMaskedLM.from_pretrained("xlm-roberta-large")
-
-        self.fill_mask_pipeline = pipeline("fill-mask", self.model, tokenizer=self.tokenizer)
-
-    def evaluate_sentences_proba(self, sentences: [str]) -> List[float]:
-
-        proba_by_sentences = []
-        for sentence in sentences:
-            tokenized_sentence: [str] = self.tokenizer.tokenize(sentence)
-            tokenized_without_underscores: [str] = [token.replace("▁", "") for token in tokenized_sentence]
-            sentence_with_masks: [str] = [sentence.replace(token, self.tokenizer.mask_token, 1) for token in tokenized_without_underscores]
-            sum_log_proba = 1.0
-            for masked_sentence, token in zip(sentence_with_masks, tokenized_without_underscores):
-                this_token_result_by_sentence = self.fill_mask_pipeline(masked_sentence, targets=token)
-                sum_log_proba += np.log(this_token_result_by_sentence[0]["score"])
-            proba_by_sentences.append(np.exp(sum_log_proba/len(tokenized_sentence)))
-        return proba_by_sentences
-
-    def evaluate_sentences_for_word(self, sentences: [str], word_to_guess: str) -> [float]:
-
-        tokenized_word_to_guess: List[str] = self.tokenizer.tokenize(word_to_guess)
-        tokenized_without_underscores: List[str] = [token.replace("▁", "") for token in tokenized_word_to_guess]
-
-        scores_all_tokens = np.ones(len(sentences))
-        for token in tokenized_without_underscores:
-            sentences_with_mask: [str] = [sentence.replace(token, self.tokenizer.mask_token, 1) for sentence in sentences]
-
-            this_token_result_by_sentence = self.fill_mask_pipeline(sentences_with_mask, targets=token)
-
-            scores = [sentence_result[0]["score"] for sentence_result in this_token_result_by_sentence]
-            scores_all_tokens *= scores
-        return scores_all_tokens.tolist()
-
-
-# TODO gerer le fait que suivant la phase le mot n est pas exactement le meme meme si cest le meme learnable word
-
-
-        # tokenized = self.tokenizer.tokenize(sentence)
-
-        # Add start and end token, tokenize then convert to ids
-        # tokenized_encoded = self.tokenizer.encode(sentence)
-
-        #ids = self.tokenizer.convert_tokens_to_ids(tokenized)
-
-        # result_mask_pipeline = self.fill_mask_pipeline(sentence)
-
-        # print(tokenized)
-
-
-s = SentenceEvaluator()
-
-sentences_tradition = [
-    "Đó là truyền thống gia đình",
-    "Đó là một truyền thống gia đình",
-    "Đó là truyền thống",
-    "Hãy tôn trọng truyền thống",
-    "Một trong những cuộc chiến quan trọng nhất là chống lại truyền thống"
-]
-
-#proba_word = s.evaluate_sentences_for_word(sentences_tradition, "truyền thống")
-#print(proba_word)
-
-#proba = s.evaluate_sentences_proba(sentences_tradition)
-#print(proba)
+                current_start_index = 1
+                log_proba_sentence = 1.0
+                for idx_word, encoded_word in enumerate(encoded_words):
+                    nb_tokens_this_word = encoded_word.shape[1]
+                    log_proba_word = 0.0
+                    encoded_word_1d_tensor = encoded_word[0,:]
+                    for idx_token_in_word, expected_token_idx in enumerate(encoded_word_1d_tensor):
+                        log_proba_word = log_proba_word + tensor_log_probs[idx_word, current_start_index + idx_token_in_word, expected_token_idx]
+                    log_proba_word /= nb_tokens_this_word
+                    log_proba_sentence += log_proba_word
+                    current_start_index += nb_tokens_this_word
+                log_proba_sentence /= nb_words
+                probas.append(np.exp(log_proba_sentence.item()))
+            return probas
 
 
 sentences = [
-    "Le bébé a fait caca dans sa couche",
-    "Plus de couches pour le bébé",
-    "Je te promet le ciel au dessus de ta couche",
-    "Le bébé fait caca dans sa couche"
+    Sentence(sentence="Le bébé a fait caca", words=["Le", "bébé", "a", "fait", "caca"]),
+    Sentence(sentence="Le bébé a fait caca dans sa couche", words=["Le", "bébé", "a", "fait", "caca", 'dans', 'sa', 'couche']),
+    Sentence(sentence="Le bébé fait caca dans sa couche", words=["Le", "bébé", "fait", "caca", 'dans', 'sa', 'couche']),
+    Sentence(sentence="Le bébé dans sa couche", words=["Le", "bébé",'dans', 'sa', 'couche']),
+    Sentence(sentence="Le bébé couche", words=["Le", "bébé", 'couche']),
+    Sentence(sentence="Bébé a fait dans sa couche", words=["bébé", "a", "fait", 'dans', 'sa', 'couche']),
+    Sentence(sentence="Caca dans sa couche", words=["caca", 'dans', 'sa', 'couche']),
 ]
-
-proba = s.evaluate_sentences_proba(sentences)
+s = SentenceEvaluatorTwo()
+proba = s.evaluate_sentence_log_proba_by_word(sentences)
 print(proba)
 
 #s.evaluate_word_in_sentence("Đó là một <mask> <mask> gia đình", "truyền thống")
