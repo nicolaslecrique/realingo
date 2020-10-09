10d9b236ba89dd5d3e3f8d0fc4bf963923c83c06
Nicolas <nicolas.lecrique+git@globers.co>
2020-10-09 11:04:39 +0200

    add language prefix for evaluator: give better results
    Former-commit-id: 10ab9def470f5af83ce6e4d522ed85ceab1e634f

--- programbuilder/sentence_evaluator.py
+++ programbuilder/sentence_evaluator.py
@@ -1,17 +1,32 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from enum import Enum
+from typing import List
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForMaskedLM, XLMRobertaTokenizer, \
     XLMRobertaForMaskedLM
 import torch
 
+class Language(Enum):
+    VIETNAMESE = 1,
+    FRENCH = 2
+
+
+def _to_prefix(language: Language) -> str:
+    if language == Language.VIETNAMESE:
+        return "Tiếng Việt. "
+    elif language == Language.FRENCH:
+        return "Français. "
+    raise ValueError("Unmanaged language: " + str(language))
+
 
 @dataclass
 class Sentence:
+    language: Language
     sentence: str
     words: [str]
     # The tokenized sentence must contains the tokenized word, and then it is counted as "one" and masked together
     # Not all the tokens in the sentence are in "words" (punctuations, proper names, onomatopoeia...)
+
 
 @dataclass
 class SentenceEvaluationResult:
@@ -19,11 +34,7 @@ class SentenceEvaluationResult:
     words_proba: [np.ndarray]
 
 
-
-# TODO: refacto to not give indexes bounds but all token indexes that should be masked
-# Will allow to test more things
-
-class SentenceEvaluatorTwo:
+class SentenceEvaluator:
 
     def __init__(self):
         self.tokenizer: XLMRobertaTokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
@@ -34,7 +45,7 @@ class SentenceEvaluatorTwo:
 
         indexes = []
         size_encoded_sentence = encoded_sentence.shape[0]
-        current_start_idx_in_sentence = 0
+        current_start_idx_in_sentence = 1 # after sos_token
         for encoded_word in encoded_words:
             size_encoded_word = encoded_word.shape[0]
             for idx_in_sentence in range(current_start_idx_in_sentence, size_encoded_sentence):
@@ -78,11 +89,12 @@ class SentenceEvaluatorTwo:
 
         with torch.no_grad():
 
-            tokenized_sentence = self.tokenizer.tokenize(sentence.sentence)
-            tokenized_words = [self.tokenizer.tokenize(word) for word in sentence.words]
+            # tokenized_sentence = self.tokenizer.tokenize(sentence.sentence)
+            # tokenized_words = [self.tokenizer.tokenize(word) for word in sentence.words]
+            prefixes_sentence = _to_prefix(sentence.language) + sentence.sentence
 
             encoded_words = [self.tokenizer.encode(word, add_special_tokens=False, return_tensors="pt")[0] for word in sentence.words]
-            encoded_sentence: torch.tensor = self.tokenizer.encode(sentence.sentence, return_tensors="pt")[0]
+            encoded_sentence: torch.tensor = self.tokenizer.encode(prefixes_sentence, return_tensors="pt")[0]
 
             min_max_token_indexes_by_word: List[np.ndarray] = self._extract_mask_indexes_in_sentence(encoded_sentence, encoded_words)
             encoded_sentence_for_masks: torch.tensor = self._build_masked_sentences(encoded_sentence, min_max_token_indexes_by_word)
@@ -91,8 +103,6 @@ class SentenceEvaluatorTwo:
             log_proba_by_word: np.ndarray = self._extract_log_proba_world_tensor_from_model_result(model_result_log_probs, min_max_token_indexes_by_word, encoded_sentence)
 
             return log_proba_by_word
-
-
 
     def compute_sentence_and_word_proba(self, sentence: Sentence) -> SentenceEvaluationResult:
         log_proba_array = self.evaluate_sentence_log_proba_by_word(sentence)
@@ -107,49 +117,24 @@ class SentenceEvaluatorTwo:
             print(word + ":" + str(proba))
 
 
-s = SentenceEvaluatorTwo()
+s = SentenceEvaluator()
+
+
+sentences = [
+    Sentence(language=Language.VIETNAMESE, sentence="Đó là truyền thống gia đình.", words=["Đó", "là", "truyền thống", "gia đình"]),
+    Sentence(language=Language.VIETNAMESE, sentence="Đó là một truyền thống gia đình.", words=["Đó", "là", "một", "truyền thống", "gia đình"]),
+    Sentence(language=Language.VIETNAMESE, sentence="Đó là truyền thống.", words=["Đó", "là", "truyền thống"]),
+    Sentence(language=Language.VIETNAMESE, sentence="Hãy tôn trọng truyền thống.", words=["Hãy", "tôn trọng", "truyền thống"]),
+]
 
 
 sentences_ = [
-    Sentence(sentence="Le bébé a fait caca.", words=["Le", "bébé", "a", "fait", "caca"]),
-    Sentence(sentence="Le bébé a fait caca dans sa couche.", words=["Le", "bébé", "a", "fait", "caca", 'dans', 'sa', 'couche']),
-    Sentence(sentence="Le bébé fait caca dans sa couche.", words=["Le", "bébé", "fait", "caca", 'dans', 'sa', 'couche']),
-    Sentence(sentence="Le bébé dans sa couche.", words=["Le", "bébé",'dans', 'sa', 'couche']),
-    Sentence(sentence="Le bébé couche.", words=["Le", "bébé", 'couche']),
-    Sentence(sentence="Le bébé se couche.", words=["Le", "bébé", 'se', 'couche']),
-    Sentence(sentence="Le bébé se couches.", words=["Le", "bébé", 'se', 'couches']),
-    Sentence(sentence="Bébé a fait dans sa couche.", words=["Bébé", "a", "fait", 'dans', 'sa', 'couche']),
-    Sentence(sentence="Caca dans sa couche.", words=["caca", 'dans', 'sa', 'couche']),
-    Sentence(sentence="Il est dans mon.", words=["il", 'est', 'dans', 'mon']),
+    Sentence(language=Language.FRENCH, sentence="Français. Il fait beau.", words=["Il", 'fait', 'beau']),
+    Sentence(language=Language.FRENCH, sentence="Français. Il fait beau aujourd'hui.", words=["Il", 'fait', 'beau', "aujourd'hui"]),
+    Sentence(language=Language.FRENCH, sentence="Français. Il fait un beau temps aujourd'hui.", words=["Il", 'fait', 'un', 'beau', 'temps', "aujourd'hui"]),
+    Sentence(language=Language.FRENCH, sentence="Français. Il est beau aujourd'hui.", words=["Il", 'est', 'beau', "aujourd'hui"]),
 ]
 
-
-sentences_viet = [
-    Sentence(sentence="Đó là truyền thống gia đình.", words=["Đó", "là", "truyền thống", "gia đình"]),
-    Sentence(sentence="Đó là một truyền thống gia đình.", words=["Đó", "là", "một", "truyền thống", "gia đình"]),
-    Sentence(sentence="Đó là truyền thống.", words=["Đó", "là", "truyền thống"]),
-    Sentence(sentence="Hãy tôn trọng truyền thống.", words=["Hãy", "tôn trọng", "truyền thống"]),
-]
-
-
-sentences__ = [
-    Sentence(sentence="Le bébé a fait caca.", words=["le", "bébé", "a", "fait", "caca"]),
-
-]
-
-sentences = [
-    Sentence(sentence="Il fait beau.", words=["Il", 'fait', 'beau']),
-    Sentence(sentence="Il fait beau aujourd'hui.", words=["Il", 'fait', 'beau', "aujourd'hui"]),
-    Sentence(sentence="Il fait un beau temps aujourd'hui.", words=["Il", 'fait', 'un', 'beau', 'temps', "aujourd'hui"]),
-    Sentence(sentence="Il est beau aujourd'hui.", words=["Il", 'est', 'beau', "aujourd'hui"]),
-]
-
-#proba = s.compute_sentence_proba(sentences)
-#print(proba)
 for sentence in sentences:
     s.print_words_proba(sentence)
     print("===")
-
-
-#s.evaluate_word_in_sentence("Đó là một <mask> <mask> gia đình", "truyền thống")
-#s.evaluate_word_in_sentence("This is really cool", "really")
