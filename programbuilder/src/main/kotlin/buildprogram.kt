038fa886a610c7ed379fbe81ad0d4b3345a11d2d
Nicolas <nicolas.lecrique+git@globers.co>
2020-10-15 09:28:02 +0200

    [programbuilder] add script to generate program (WIP)
    Former-commit-id: 41ebd72a54d7ef70dbabb5bca8fe7ab120db2f7c

--- programbuilder/src/main/kotlin/buildprogram.kt
+++ programbuilder/src/main/kotlin/buildprogram.kt
@@ -38,10 +38,36 @@ class FileLoader {
 
 }
 
+fun computeScore(sentence: SentenceData, learnableWord: String) =
+    sentence.sentence_probability * sentence.words_in_sentence.first { it.word_standard_format == learnableWord }.word_probability_in_sentence
+
 
 fun main() {
 
-    val languageDataStr = FileLoader.getFileFromResource("./language_data/vietnamese/language_data.json").readText()
+    val languageDataStr = FileLoader.getFileFromResource("./language_data/vietnamese/language_data_10000.json").readText()
     val languageData = Json.decodeFromString<LanguageData>(languageDataStr)
+
+    val countByWord = mutableMapOf<String,Int>()
+    for (sentence in languageData.sentences){
+        for (word in sentence.words_in_sentence){
+            val currentCount: Int = countByWord.getOrDefault(word.word_standard_format, 0)
+            countByWord[word.word_standard_format] = currentCount + 1
+        }
+    }
+    // associate each sentence to its less frequent word in the sentence
+    // then filter the 100 best sentences for each word
+    val learnableWordToSentences = languageData.sentences
+        .groupBy { sentence -> sentence.words_in_sentence.map { it.word_standard_format }.minByOrNull{ countByWord.getValue(it) }!! }
+        .mapValues { pair -> pair.value.sortedBy { -computeScore(it,pair.key) }.take(100) }
+
+
+
+
+
+    // 1) faire la table de frequence des mots
+    // 2) associer à chaque mot ses phrases
+    // 3) trier les phrases par qualité
+
+
     println("Hello World")
 }
