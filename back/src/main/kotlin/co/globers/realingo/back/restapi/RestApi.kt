dc1010e2476a90b2be6387bb1a61eb6298b1056c
Nicolas <nicolas.lecrique+git@globers.co>
2020-10-22 11:14:54 +0200

    add init backend rest for the app
    Former-commit-id: 01edc87311b194515ad1b893bd12a6f9b48bb43c

---
+++ back/src/main/kotlin/co/globers/realingo/back/restapi/RestApi.kt
@@ -0,0 +1,32 @@
+package co.globers.realingo.back.restapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class Language(
+        val languageUri: String,
+        val languageLabel: String
+)
+
+val availableOriginLanguages: List<Language> = listOf(
+        Language("french", languageLabel = "French")
+)
+
+val availableTargetLanguages: List<Language> = listOf(
+        Language("vietnamese", languageLabel = "Vietnamese")
+)
+
+@RestController
+class RestApi {
+
+    @GetMapping("/available_origin_languages")
+    suspend fun getAvailableOriginLanguages(): List<Language> {
+        return availableOriginLanguages
+    }
+
+    @GetMapping("/available_target_languages")
+    suspend fun getAvailableTargetLanguages(): List<Language> {
+        return availableTargetLanguages
+    }
+
+}
