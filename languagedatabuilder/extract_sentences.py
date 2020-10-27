71401fe11e0994e40192596115d517f63a1e0448
Nicolas <nicolas.lecrique+git@globers.co>
2020-10-27 22:17:57 +0100

    use ascii (or it crashes when run in collab on deserialization, i don't know why)
    Former-commit-id: 4fb6e2636e5376324ecffd535fe21ba401c9f225

--- languagedatabuilder/extract_sentences.py
+++ languagedatabuilder/extract_sentences.py
@@ -5,10 +5,10 @@ from languages_toolboxes.api_language_toolbox import ExtractedSentences
 from languages_toolboxes.vietnamese_toolbox import VietnameseToolbox
 
 with VietnameseToolbox() as viet_toolbox:
-    sentences: ExtractedSentences = build_extracted_sentences("vietnamese", viet_toolbox, nb_lines=1000)
+    sentences: ExtractedSentences = build_extracted_sentences("vietnamese", viet_toolbox, nb_lines=10000000)
     json_sentences = sentences.to_dict()
 
     print("start dump")
-    with open("temp/extracted_sentences_vietnamese_200.json", 'w') as dump_file_tokenized:
-        json.dump(json_sentences, dump_file_tokenized, indent=2, ensure_ascii=False)
+    with open("temp/extracted_sentences_vietnamese_10000000_ascii.json", 'w') as dump_file_tokenized:
+        json.dump(json_sentences, dump_file_tokenized, indent=2, ensure_ascii=True)
     print("end dump")
