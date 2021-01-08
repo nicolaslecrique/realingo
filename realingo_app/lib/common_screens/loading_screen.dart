340c05b462df0b969fcbcbfd659d7dddf475a4f6
Nicolas <nicolas.lecrique+git@globers.co>
2021-01-08 11:21:25 +0100

    design loading screen
    Former-commit-id: 973c18c3dd09e3f61c17e8a63c6ebf8828896271

--- realingo_app/lib/common_screens/loading_screen.dart
+++ realingo_app/lib/common_screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:realingo_app/design/constants.dart';
 
 @immutable
 class LoadingScreen extends StatelessWidget {
@@ -6,10 +7,31 @@ class LoadingScreen extends StatelessWidget {
 
   const LoadingScreen({Key key, this.message}) : super(key: key);
 
+  // #57B4FA
+  // #1696F7
+  // #93BCF5
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(StandardSizes.medium),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(message + '...', style: StandardFonts.bigFunny),
+            SizedBox(height: 2 * StandardSizes.medium),
+            SizedBox(height: 2 * StandardSizes.medium),
+            ClipRRect(
+                // https://stackoverflow.com/questions/57534160/how-to-add-a-border-corner-radius-to-a-linearprogressindicator-in-flutter
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: LinearProgressIndicator(
+                  minHeight: StandardSizes.medium,
+                ))
+          ],
+        )),
+      ),
     );
   }
 }
