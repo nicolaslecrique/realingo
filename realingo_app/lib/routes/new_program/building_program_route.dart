13675be1fb977103aaa98d504571b54c7fc987b0
Nicolas <nicolas.lecrique+git@globers.co>
2021-01-08 16:05:26 +0100

    bypass level selection for MVP
    Former-commit-id: 578675c8f2496a3c2ac28ae777664d66aec6d45a

--- realingo_app/lib/routes/new_program/building_program_route.dart
+++ realingo_app/lib/routes/new_program/building_program_route.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:realingo_app/common_screens/loading_screen.dart';
 import 'package:realingo_app/model/program.dart';
-import 'package:realingo_app/routes/new_program/select_level_route.dart';
+import 'package:realingo_app/model/user_program_model.dart';
+import 'package:realingo_app/routes/home/home_route.dart';
 import 'package:realingo_app/services/program_services.dart';
 
 // Screen to build a new program, so everything is in local when displaying program_route (as if it comes from splash_screen)
@@ -29,11 +31,15 @@ class _BuildingProgramRouteState extends State<BuildingProgramRoute> {
   @override
   void initState() {
     super.initState();
+    _buildProgram().then((value) => Navigator.pushNamedAndRemoveUntil(context, HomeRoute.route, (r) => false));
+  }
 
-    Future<LearningProgram> futureProgram =
-        ProgramServices.getProgram(widget.args.learnedLanguage, widget.args.originLanguage);
-    futureProgram
-        .then((value) => Navigator.pushNamed(context, SelectLevelRoute.route, arguments: SelectLevelRouteArgs(value)));
+  Future<void> _buildProgram() async {
+    LearningProgram program = await ProgramServices.getProgram(widget.args.learnedLanguage, widget.args.originLanguage);
+
+    await ProgramServices.buildUserProgram(program, program.itemsToLearn.first);
+    UserProgramModel model = Provider.of<UserProgramModel>(context, listen: false);
+    await model.reload();
   }
 
   @override
