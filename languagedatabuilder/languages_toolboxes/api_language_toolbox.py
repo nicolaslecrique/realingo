fa12b27f825b0282f6a2522ec4c47dffbc0688cd
Nicolas <nicolas.lecrique+git@globers.co>
2021-02-02 22:04:44 +0100

    add info about pos tags for vietnamese
    Former-commit-id: ca6926cb135fb5608bab77163096f7f55c6692fd

--- languagedatabuilder/languages_toolboxes/api_language_toolbox.py
+++ languagedatabuilder/languages_toolboxes/api_language_toolbox.py
@@ -3,6 +3,42 @@ from typing import List
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
+# Label 	Meaning
+# Np 	Proper noun
+# Nc 	Classifier noun
+# Nu 	Unit noun
+# N 	Noun
+# Ny 	Abbreviated noun
+# Nb 	(Foreign) borrowed noun
+# V 	Verb
+# Vb 	(Foreign) borrowed verb
+# A 	Adjective
+# P 	Pronoun
+# R 	Adverb
+# L 	Determiner
+# M 	Numeral/Quantity
+# E 	Preposition
+# C 	Subordinating conjunction
+# Cc 	Coordinating conjunction
+# I 	Interjection/Exclamation
+# T 	Particle/Auxiliary, modal words
+# Y 	Abbreviation
+# Z 	Bound morpheme
+# X 	Un-definition/Other
+# CH 	Punctuation and symbols
+
+#exclude_pos_tags = {'Np', 'Ny', 'Nb', 'Vb', 'Y', 'Z', 'CH'}
+# M: if it's nto digits  : don't add for now
+
+
+#digit_pos_tag = 'M'
+#proper_noun_pos_tags = 'Np'
+#punctuation_symbols_pos_tags = 'CH'
+#normal_words_pos_tags = {'Nc', 'Nu', 'N', 'V', 'A', 'P', 'R', 'L', 'E', 'C', 'Cc', 'I', 'T', 'X', 'Z'}
+#exclude_sentence_pos_tags = {'Ny', 'Nb', 'Vb', 'Y'}
+
+#not_excluded_sentence_punctuation_symbols_pos = {',', ';', '.', '!', '?'}
+
 
 class Language(Enum):
     VIETNAMESE = 1,
