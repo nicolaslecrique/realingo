629f891172d6f5576789d9302e095f761905b14b
Nicolas <nicolas.lecrique+git@globers.co>
2021-02-12 10:41:15 +0100

    [Translator] Add comments run script on distant VM + set correct batch size and file extention
    Former-commit-id: 8ed088b588704247576fa531791f3455ca90d12f

--- translator/main.py
+++ translator/main.py
@@ -8,15 +8,32 @@ from typing import List
 
 from translator import Translator, TranslatedSentence
 
+# NB: Se connecter en ssh: ssh nicolas@35.227.175.180
+# copy files
+# gcloud compute scp main.py deeplearning-1-vm:~/translator/main.py
+# gcloud compute scp translation_evaluator.py deeplearning-1-vm:~/translator/translation_evaluator.py
+# gcloud compute scp translator.py deeplearning-1-vm:~/translator/translator.py
+# gcloud compute scp requirements.txt deeplearning-1-vm:~/translator/requirements.txt
+# gcloud compute scp --recurse data deeplearning-1-vm:~/translator/data
+
+# Launch so that it continues even after ssh session end
+# nohup python main.py &
+
+# see if it is still running as it should
+# wc -l ./tmp/vietnamese/open_subtitles_translated.jsonl
+
+# To dowload generated file from VM
+# gcloud compute scp nicolas@deeplearning-1-vm:~/translator/tmp/vietnamese/open_subtitles_translated.jsonl .
+
 with open("./data/vietnamese/open_subtitles.txt") as sentences_file:
     lines: List[str] = sentences_file.read().splitlines()
 
 translator: Translator = Translator("vi", "en")
 
-batch_size = 2
+batch_size = 16
 
-cache_to_print_size = 4
-dest_file: str = "./tmp/vietnamese/open_subtitles_translated.txt"
+cache_to_print_size = 32
+dest_file: str = "./tmp/vietnamese/open_subtitles_translated.jsonl"
 current_line_index = 0
 if os.path.exists(dest_file):
     with open(dest_file, encoding="utf-8") as translated_file:
