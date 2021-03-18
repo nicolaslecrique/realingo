7c941a3bfb13ccf346e4e49a880812a18ecdf517
Nicolas <nicolas.lecrique+git@globers.co>
2021-03-18 11:06:53 +0100

    adapt user preference to store default program and last completed lesson
    step to remove db
    
    
    Former-commit-id: 4a6614732bb22e1ae6f2c110b6d98945592a9426

--- realingo_app/lib/tech_services/user_config.dart
+++ realingo_app/lib/tech_services/user_config.dart
@@ -1,23 +1,47 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+@immutable
+class ProgramState {
+  final String programUri;
+  final String lastCompletedLessonUriOrNull;
+
+  const ProgramState(this.programUri, this.lastCompletedLessonUriOrNull);
+}
+
 class UserConfig {
+  static const String _defaultProgramUriKey = 'default_program_uri';
+  static String _getLastCompletedLessonUriKey(String programUri) => '${programUri}/last_completed_lesson_uri';
+
   static Future<void> clear() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.clear();
   }
 
-  static Future<String> getDefaultUserProgramUriOrNull() async {
+  static Future<ProgramState> getDefaultProgramStateOrNull() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    if (prefs.containsKey('default_user_program_uri')) {
-      return prefs.getString('default_user_program_uri');
-    } else {
+    if (!prefs.containsKey(_defaultProgramUriKey)) {
       return null;
+    }
+
+    String programUri = prefs.getString(_defaultProgramUriKey);
+    String lessonKey = _getLastCompletedLessonUriKey(programUri);
+    if (!prefs.containsKey(lessonKey)) {
+      String lessonUri = prefs.getString(lessonKey);
+      return ProgramState(programUri, lessonUri);
+    } else {
+      return ProgramState(programUri, null);
     }
   }
 
-  static Future<void> setDefaultUserProgramUri(String uri) async {
+  static Future<void> setDefaultProgram(String programUri) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('default_user_program_uri', uri);
+    await prefs.setString(_defaultProgramUriKey, programUri);
+  }
+
+  static Future<void> setLastCompletedLessonUri(String programUri, String lessonUri) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String lessonKey = _getLastCompletedLessonUriKey(programUri);
+    await prefs.setString(lessonKey, lessonUri);
   }
 }
