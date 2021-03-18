cbbef0d5a300a99c689ef5f7a9b175f33a35775e
Nicolas <nicolas.lecrique+git@globers.co>
2021-03-18 11:10:38 +0100

    Remove sqlite code & dependency
    We will move everything to the backend for now
    frontend will only be a simple cache that could be lost and reloaded from server
    => we can use hive for this
    
    
    Former-commit-id: 6d12df3eb2fc9563ec0e9ef889e067ab35d690bf

--- realingo_app/lib/tech_services/database/schema.dart
+++
@@ -1,9 +0,0 @@
-import 'package:realingo_app/tech_services/database/table/user_item_sentence.dart';
-import 'package:realingo_app/tech_services/database/table/user_item_to_learn.dart';
-import 'package:realingo_app/tech_services/database/table/user_learning_program.dart';
-
-class DB {
-  static const TableUserItemToLearn userItemToLearn = TableUserItemToLearn();
-  static const TableUserLearningProgram userLearningProgram = TableUserLearningProgram();
-  static const TableUserItemSentence userItemSentence = TableUserItemSentence();
-}
