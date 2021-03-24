580efc24dbc2511d9afebfe40d9368618d1b253d
Nicolas <nicolas.lecrique+git@globers.co>
2021-03-24 11:17:02 +0100

    Load sentence records when lesson is loaded
    so it can be played immediately
    
    
    Former-commit-id: c0bb24fb8a5fb2e04a258c3a2fe8aa55d23202d6

--- realingo_app/lib/model/user_program_model.dart
+++ realingo_app/lib/model/user_program_model.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:realingo_app/model/user_program.dart';
 import 'package:realingo_app/services/program_services.dart';
+import 'package:realingo_app/services/texttospeech_service.dart';
 
 class UserProgramModel extends ChangeNotifier {
   UserLearningProgram _programOrNull;
 
   Future<void> reload() async {
     _programOrNull = await ProgramServices.getDefaultUserProgramOrNull();
+    if (_programOrNull != null) {
+      await TextToSpeech.loadSentences(_programOrNull.program.learnedLanguageUri, _programOrNull.nextLesson.sentences);
+    }
+
     notifyListeners();
   }
 
--- realingo_app/lib/services/texttospeech_service.dart
+++ realingo_app/lib/services/texttospeech_service.dart
@@ -7,18 +7,27 @@ import 'package:realingo_app/model/program.dart';
 import 'package:realingo_app/tech_services/rest/rest_api.dart';
 
 class TextToSpeech {
+  static Future<void> loadSentences(String languageUri, List<Sentence> sentences) async {
+    final Directory directory = await getApplicationSupportDirectory();
+    await Future.wait(sentences.map((e) => _loadOrGetSentenceRecordFilePath(directory, e, languageUri)));
+  }
+
   static Future<void> play(String languageUri, Sentence sentence) async {
     final Directory directory = await getApplicationSupportDirectory();
+    String path = await _loadOrGetSentenceRecordFilePath(directory, sentence, languageUri);
+    await _playFromFile(path);
+  }
+
+  static Future<String> _loadOrGetSentenceRecordFilePath(
+      Directory directory, Sentence sentence, String languageUri) async {
     String path = '${directory.absolute.path}/sentence_records/${sentence.uri}.wav';
     File file = File(path);
-    if (await file.exists()) {
-      await _playFromFile(path);
-    } else {
+    if (!await file.exists()) {
       final Uint8List wavContent = await RestApi.getRecord(languageUri, sentence.sentence);
       await file.create(recursive: true); // in case sentence_records directory doesn't exist
       await file.writeAsBytes(wavContent, mode: FileMode.writeOnly, flush: true);
-      await _playFromFile(path);
     }
+    return path;
   }
 
   static Future<void> _playFromFile(String url) async {
